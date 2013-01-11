dd90765172fe9f426d536ca339e72bd4b30720a5
R. Mathieu Landvreugd (KLM) <mathieu.landvreugd@klm.com>
2013-01-11 13:05:12 +0100

    Prepare for 'Views'

--- js/main.js
+++ js/main.js
@@ -3,20 +3,5 @@ var Person = Backbone.Model.extend({
 		name: 'John Doe',
 		age: 38,
 		occupation: 'worker'
-	},
-
-	validate: function(attrs){
-		if ( attrs.age < 0 ) {
-			return 'Age must be positive!';
-		}
-
-		if ( ! attrs.name) {
-			return 'Every person must have a name!';
-		}
-	},
-
-	work: function() {
-		return this.get('name') + ' is working.';
 	}
-
 });
