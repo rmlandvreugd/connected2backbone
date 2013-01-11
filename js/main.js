35700224a974e127264666d809bbf950fa82e556
R. Mathieu Landvreugd (KLM) <mathieu.landvreugd@klm.com>
2013-01-11 11:33:03 +0100

    Reformatting.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 var Person = Backbone.Model.extend({
-    defaults:{
-        name: 'John Doe',
-        age: 38,
-        occupation: 'worker'
-    }
+	defaults: {
+		name: 'John Doe',
+		age: 38,
+		occupation: 'worker'
+	},
+	work: function() {
+		return this.get('name') + ' is working.';
+	}
 
 });
