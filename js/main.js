3480fcfffa3b6d33e13d4c01a6b09d3652415b8c
R. Mathieu Landvreugd (KLM) <mathieu.landvreugd@klm.com>
2013-01-11 14:24:30 +0100

    The 'person' view rendering the default model on init

--- js/main.js
+++ js/main.js
@@ -10,13 +10,16 @@ var PersonView = Backbone.View.extend({
   tagName: 'li',
 
   initialize: function() {
-    console.log(this.model);
+    console.log("'initialize()': with model ");
+    console.log(this.model.toJSON());
+    this.render();
   },
 
   render: function() {
-
+    this.$el.html( this.model.get('name') );
+    return this;
   }
 });
 
-var person = new Person;
+var person = new Person();
 var personView = new PersonView({ model: person });
