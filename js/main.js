18c9eff63a1f2f43068be413ff38e976529b89a6
R. Mathieu Landvreugd (KLM) <mathieu.landvreugd@klm.com>
2013-01-11 13:33:56 +0100

    The 'person' view rendering the default model

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 var Person = Backbone.Model.extend({
-	defaults: {
-		name: 'John Doe',
-		age: 38,
-		occupation: 'worker'
-	}
+  defaults: {
+    name: 'John Doe',
+    age: 38,
+    occupation: 'worker'
+  }
 });
 
 var PersonView = Backbone.View.extend({
   tagName: 'li',
 
   initialize: function() {
-    console.log('Hi there!');
+    console.log(this.model);
   },
 
-  render: function(){
-    
+  render: function() {
+
   }
 });
+
+var person = new Person;
+var personView = new PersonView({ model: person });
