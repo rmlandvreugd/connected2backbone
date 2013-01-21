9badcc093417e62dc7df75ffc2b37645583c66ce
R. Mathieu Landvreugd (KLM) <mathieu.landvreugd@klm.com>
2013-01-21 14:15:53 +0100

    * js/main.js: Routes added for 'index' and 'show'. * README.md: Finished 'Routes 101'.

--- js/main.js
+++ js/main.js
@@ -12,4 +12,23 @@ window.template = function(id) {
   return _.template( $('#' + id).html() );
 };
 
+App.Router = Backbone.Router.extend({
+
+  routes: {
+    '': 'index',
+    'show': 'show'
+  },
+
+  index: function(){
+    console.log("App.Router: index()");
+  },
+
+  show: function(){
+    console.log("App.Router: show()");
+  }
+});
+
+new App.Router;
+Backbone.history.start();
+
 })();
